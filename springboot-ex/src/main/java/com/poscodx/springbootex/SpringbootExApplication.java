4e5fb31f0518156e1e67352fa3d68ae73da97764
s2lilac <s2cherryy@gmail.com>
2023-10-10 17:59:42 +0900

    etc: commit test

--- springboot-ex/src/main/java/com/poscodx/springbootex/SpringbootExApplication.java
+++ springboot-ex/src/main/java/com/poscodx/springbootex/SpringbootExApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootExApplication {
 
     public static void main(String[] args) {
+
+//        run 에서 일어나는 일
+//        1. 어플리케이션 컨텍스트 (Application Context / Spring Container) 생성
+//        2.
         SpringApplication.run(SpringbootExApplication.class, args);
     }
 
